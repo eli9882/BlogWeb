3bf3e37c75a8a657cabce442e2f2af5aee2a2a95
Liza988 <140469042+eli9882@users.noreply.github.com>
2024-06-01 15:51:56 -0600

    addLayout.jsx

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const Header = ()=> {
+    return (
+        <nav>
+            <div className='container nav_container'>
+                <link to="/"  className='nav_logo'>
+                </link>
+            </div>
+        </nav>
+    )
+}
+export default Header

5d98c0cf6a346ee5cbddff2b7e72970af6488dcf
Liza988 <140469042+eli9882@users.noreply.github.com>
2024-06-01 23:15:57 -0600

    ViewPost

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import Logo from '../images/logoblog.png'
+import {FaBars} from "react-icons/fa"
+import {AiOutlineClose} from "react-icons/ai"
+
 const Header = ()=> {
     return (
         <nav>
             <div className='container nav_container'>
-                <link to="/"  className='nav_logo'>
-                </link>
+                <Link to="/"  className='nav_logo'>
+                    <img src={Logo} alt="Navbar Logo"/>
+                </Link>
+                <ul className='nav_menu' >
+                  
+                  <li><Link to="/profile"> Sofia Loria </Link></li>
+                  <li> <Link to="/create"> Create Post </Link></li>
+                  <li><Link to="/authors"> Authors </Link></li>
+                  <li> <Link to="/logout"> Logout </Link></li>
+                 
+                </ul>
+                <button className='nav-toggle-btn'>
+                    <AiOutlineClose/>
+                </button>
             </div>
         </nav>
     )

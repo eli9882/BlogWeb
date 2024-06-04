59c72899c59525a4168306e2038b1395903f6a22
Liza988 <140469042+eli9882@users.noreply.github.com>
2024-06-03 20:40:47 -0600

    Profile

--- client/src/pages/Author.jsx
+++ client/src/pages/Author.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import Avatar1 from '../images/avatar1.jpg'
+import Avatar2 from '../images/Gong.jpeg'
+import Avatar3 from '../images/Toro.jpeg'
+
+const authorsData = [
+{id:1, avatar: Avatar1, name: 'Sofia Loria', posts: 3 },
+{id:2, avatar: Avatar2, name: 'Gong', posts: 5 },
+{id:3, avatar: Avatar3, name: 'Toro', posts: 1 }
+]
+
 const Author = ()=> {
+    const [author, setAuthor] = useState(authorsData)
     return (
-        <div>Author</div>
+        <section className='authors'>
+            {author.length > 0 ? <div className='container authors_container'>
+                {
+                    author.map(({id, avatar, name, posts}) =>{
+                        return <Link to={`/posts/users/${id}`} className='author'>
+                            <div className='author_avatar'>
+                                <img src={avatar} alt={`Image of ${name}`}/>
+                            </div>
+                            <div className='author_info'>
+                                <h4>{name}</h4>
+                                <p>{posts}</p>
+                            </div>
+                        </Link>
+                    })
+                }
+            </div> : <h2 className='center'>No users/ author found.</h2>}
+        </section>
     )
 }
 export default Author
--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import {Link} from 'react-router-dom'
+import Avatar from '../images/avatar1.jpg'
+import {FaEdit} from "react-icons/fa";
+import {FaCheck} from "react-icons/fa"
+
 const UserProfile = ()=> {
+    const [ avatar , setAvatar] = useState('')
     return (
-        <div>UserProfile</div>
+       <section className='profile'> 
+        <div className='container profile_container'>
+            <Link to={`/mypost/sdfsff`} className='btn'>My posts</Link>
+            <div className='profile_details'>
+                <div className='avatar_wrapper'>
+                <div className='profile_avatar'>
+                <img src={Avatar} alt=''/>
+                </div>
+                {/*Form to updtae avatar*/}
+                <form className='avatar_form'>
+                    <input type="file" name="avatar" id="avatar" onChange={e => setAvatar(e.target.files[0])} accept='png, jpg, jpeg'/>
+                    <label htmlFor='avatar'><FaEdit/></label>
+                </form>
+                <button className='profile_avatar-btn'><FaCheck/></button>
+                </div>
+                </div>
+                </div>
+            </section>
     )
 }
 export default UserProfile
